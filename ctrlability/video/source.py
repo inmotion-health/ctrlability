8da6f6669159670478d8a9d97e15212d28aff514
Fabian Klinke <fabian.s.klinke@icloud.com>
2023-09-27 16:54:44 +0200

    refactored slightly and added type hints
    Signed-off-by: Fabian Klinke <fabian.s.klinke@icloud.com>

--- ctrlability/video/source.py
+++ ctrlability/video/source.py
@@ -1,50 +1,51 @@
+from typing import Optional, Tuple
 import cv2
 import imageio
 import logging as log
-
+import numpy as np
 from ctrlability.video.source_resolution_provider import find_best_resolution
+
+DEFAULT_RESOLUTION = (1280, 720)
+DEFAULT_FPS = 30
 
 
 class VideoSource:
-    def __init__(self, camera_id, width, height):
+    def __init__(self, camera_id: int, width: int, height: int):
         self.reader = None
         self.width = width
         self.height = height
-
         self.change_camera(camera_id)
 
     def __iter__(self):
         return self
 
-    def __next__(self):
+    def __next__(self) -> np.array:
         frame = self.reader.get_next_data()
 
         if frame is None:
             raise StopIteration
 
-        frame = self.flip(frame)
+        return self.flip(frame)
 
-        return frame
-
-    def flip(self, frame):
+    @staticmethod
+    def flip(frame: np.array) -> np.array:
         return cv2.flip(frame, 1)
 
+    def close(self):
+        if self.reader:
+            self.reader.close()
+
     def __del__(self):
-        self.reader.close()
+        self.close()
 
-    def change_camera(self, camera_id):
-        self._camera_id = camera_id
-        resolution, fps = find_best_resolution(camera_id)
-
-        if resolution is None:  # Fallback to 720p @ 30 FPS, which is the default for most webcams
-            resolution = ((1280, 720), 30)
+    def change_camera(self, camera_id: int):
+        resolution, fps = find_best_resolution(camera_id) or (DEFAULT_RESOLUTION, DEFAULT_FPS)
 
         log.info(f"Using resolution {resolution} and FPS: {fps} for camera {camera_id}.")
 
-        # let's pass the framerate directly to ffmpeg to avoid issues with higher frame-rates on macOS
         self.reader = imageio.get_reader(
             f"<video{camera_id}>",
-            size=(resolution[0], resolution[1]),
+            size=resolution,
             input_params=[
                 "-framerate",
                 f"{fps}",
