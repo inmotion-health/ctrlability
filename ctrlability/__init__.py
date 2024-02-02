e6400a9243837c58c24114339c8f6d02b1aeeb70
Fabian Klinke <fabian.s.klinke@icloud.com>
2024-02-02 12:19:08 +0100

    Add get_version() function to retrieve version from pyproject.toml

--- ctrlability/__init__.py
+++ ctrlability/__init__.py
@@ -1,6 +1,21 @@
-__version__ = "0.1.0"
+import toml
+import os
 
 import ctrlability.streams
 import ctrlability.processors
 import ctrlability.actions
 import ctrlability.triggers
+
+
+def get_version():
+    here = os.path.abspath(os.path.dirname(__file__))
+    pyproject_path = os.path.join(here, "..", "pyproject.toml")
+    with open(pyproject_path, "r") as pyproject:
+        pyproject_data = toml.load(pyproject)
+    return pyproject_data["project"]["version"]
+
+
+__version__ = get_version()
+
+
+__all__ = ["__version__"]
